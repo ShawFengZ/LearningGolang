bf53b7d5fbbc3395982080a2bf6c99b4da76369e
rainInteger <xiaofengz5@163.com>
2020-03-29 12:31:22 +0800

    add constant

---
+++ src/github.com/ShawFengZ/LearningGolang/06_constants/01_contant/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+//a CONSTANT is a simple unchanging value
+const p = "rain"
+
+func main() {
+	const q = 32
+	fmt.Println("p -", p)
+	fmt.Println("q -", q)
+}
---
+++ src/github.com/ShawFengZ/LearningGolang/06_constants/03_iota/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//iota是golang语言的常量计数器,只能在常量的表达式中使用。
+const (
+	a = iota //0
+	b = iota //1
+	c = iota //2
+)
+
+func main() {
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+}
